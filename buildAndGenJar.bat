f299d711753fb3a10d8f9ac9d74dcf8b46215bb8
catvod <>
2021-09-24 10:07:43 +0800

    Build script fix & Add github action support.

--- buildAndGenJar.bat
+++ buildAndGenJar.bat
@@ -1,8 +1,6 @@
 @echo off
 
-call "%~dp0\gradlew" assembleRelease
-
-del "%~dp0\custom_spider.jar"
+call "%~dp0\gradlew" assembleRelease --no-daemon
 
 call "%~dp0\jar\genJar.bat"
 
--- jar/genJar.bat
+++ jar/genJar.bat
@@ -1,12 +1,13 @@
 @echo off
 
-set dexFile="%~dp0\..\app\build\intermediates\dex\release\minifyReleaseWithR8\classes.dex"
-
+del "%~dp0\custom_spider.jar"
 rd /s/q "%~dp0\Smali_classes"
 
-java -jar "%~dp0\3rd\baksmali-2.5.2.jar" d %dexFile% -o "%~dp0\Smali_classes"
+java -jar "%~dp0\3rd\baksmali-2.5.2.jar" d "%~dp0\..\app\build\intermediates\dex\release\minifyReleaseWithR8\classes.dex" -o "%~dp0\Smali_classes"
 
 rd /s/q "%~dp0\spider.jar\smali\com\github\catvod\spider"
+
+if not exist "%~dp0\spider.jar\smali\com\github\catvod\" md "%~dp0\spider.jar\smali\com\github\catvod\"
 
 move "%~dp0\Smali_classes\com\github\catvod\spider" "%~dp0\spider.jar\smali\com\github\catvod\"
 
@@ -14,13 +15,9 @@ rd /s/q "%~dp0\Smali_classes"
 
 java -jar "%~dp0\3rd\apktool_2.4.1.jar" b "%~dp0\spider.jar" -c
 
-move "%~dp0\spider.jar\dist\dex.jar" "%~dp0\..\custom_spider.jar"
+move "%~dp0\spider.jar\dist\dex.jar" "%~dp0\custom_spider.jar"
 
 rd /s/q "%~dp0\spider.jar\smali\com\github\catvod\spider"
 
 rd /s/q "%~dp0\spider.jar\build"
 rd /s/q "%~dp0\spider.jar\dist"
-
-pause
-
-
